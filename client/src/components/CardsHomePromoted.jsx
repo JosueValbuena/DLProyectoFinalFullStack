3110b5ff5a956584dff9fe670d8464340c90556e
Josue <joshvlbn@gmail.com>
2023-08-27 20:38:31 -0400

    consulta de productos mas vendidos agregada y funcionando

--- client/src/components/CardsHomePromoted.jsx
+++ client/src/components/CardsHomePromoted.jsx
@@ -5,17 +5,17 @@ const CardsHomePromoted = ({data}) => {
 
     const navigate = useNavigate();
 
-    const handleClickNavigate = () =>{
+    const handleClickNavigate = () => {
         navigate(`/item-detail/${data.id}`)
     }
 
     return (
-    <div className='cardsHomePromoted' onClick={handleClickNavigate}>
-        <img className='cardsHomePromoted-img' src={data.img} alt="" />
-        <h3>{data.titulo}</h3>
-        <p>{data.precio}</p>
-    </div>
-  )
+        <div className='cardsHomePromoted' onClick={handleClickNavigate}>
+            <img className='cardsHomePromoted-img' src={data.img} alt="" />
+            <h3>{data.titulo}</h3>
+            <p>{data.precio}</p>
+        </div>
+    )
 }
 
 export default CardsHomePromoted
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,19 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { DataContext } from '../context/DataContext';
 import CardsHome from '../components/CardsHome';
 import SliderHome from '../components/SliderHome';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+
+    const [dataPromoted, setDataPromoted] = useState([]);
+
     const { data, getData } = useContext(DataContext);
 
+    const getProdutcsPromoted = async () => {
+        const data = await axios.get("http://localhost:3001/publicacionesPromoted");
+        setDataPromoted(data.data);
+    }
+
     useEffect(() => {
-      getData()
+        getData()
+        getProdutcsPromoted();
     }, [])
+
 
     return (
         <div>
-            <SliderHome data={data}/>
+            <SliderHome data={dataPromoted} />
             <div className='home'>
                 {data.map((ele) => {
                     return (
