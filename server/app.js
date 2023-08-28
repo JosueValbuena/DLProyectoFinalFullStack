e9da978cd812f4b43f2ed92dd3288232c7888324
FelipeDuarte28 <114889470+FelipeDuarte28@users.noreply.github.com>
2023-08-27 22:58:28 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 const router = require("./router/routers.js")
 const cors = require("cors");
 
